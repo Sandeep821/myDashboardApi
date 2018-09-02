185fb4d4e75ecc55d819f3c577e0374e02f16536
Sandeep Soni <ssoni@sandeeps-imac.lan>
2018-09-02 00:24:41 -0500

    myDasboard first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/hello', function(req, res) {
+  var data = {express: 'data from new Express'}
+ return res.send(data);
+});
+
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
+});
